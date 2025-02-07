b45338fed3b61ba087535d0717bf96609cb626d9
BakedPotato <90150681+szybkirouterzyxel@users.noreply.github.com>
2025-02-07 23:46:03 +0100

    Add files via upload

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner min = new Scanner(System.in);
-        System.out.println("JCompare is a text comparison tool made in Java");
-        System.out.println("Input first text");
+        System.out.println("JCompare is a text comparison tool made in Java, it currently isnt case sensitive but soon will be");
+        System.out.println("input the first text");
+
         String input1 = min.nextLine();
-        System.out.println("Input the second text");
+
+        System.out.println("input the second text");
+
         String input2 = min.nextLine();
-        if (input1.trim().equals(input2.trim())) {
-            System.out.println("Text matches!");
-            System.out.println("First input: " + input1);
-            System.out.println("second input: " + input2);
+
+        if (input1.equals(input2.trim())) {
+            System.out.println("Text Matches!");
+
+        } else {
+            System.out.println("Text doesnt match!");
         }
-        else {
-            System.out.println("text doesnt match!");
-            System.out.println("First input: " + input1);
-            System.out.println("second input: " + input2);
-            System.out.println("if this is a checksum, please redownload the file!");
-        }
+
+
     }
 }
